d11bb13d7d823c9b5807fcf7e6ec5087feab4ab2
Mirna Krpić <90164011+mirna-k@users.noreply.github.com>
2022-03-18 23:42:36 +0100

    Add files via upload

--- Raccoon Rickey/ClassFile.py
+++ Raccoon Rickey/ClassFile.py
@@ -23,6 +23,13 @@ class Platform(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos, speed, state):
         super().__init__()
+        self.import_character_assets()
+        self.currentSprite = 0
+        self.animation_speed = 0.25
+        self.image = self.animations['idle'][self.currentSprite]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x_pos, y_pos)
+        
         self.speed = speed
         self.movement = 0
         self.state = state
@@ -36,73 +43,25 @@ class Player(pygame.sprite.Sprite):
             self.run_state = True
 
     def import_character_assets(self):
-	    path = 'raccoon/'
+        path = 'raccoon/'
         self.animations = {'idle':[],'run':[],'jump':[]}
-
-		for animation in self.animations.keys():
-			full_path = path + animation
-			self.animations[animation] = import_images_from_folder(full_path)
-
-
-        # Idle sprites
-        self.idle = []
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0001.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0003.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0005.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0007.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0009.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0011.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0013.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0015.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0017.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0019.png')))
-        self.idle.append(pygame.image.load(
-            os.path.join('raccoon/idle', '0021.png')))
         
-        # Run sprites
-        self.run = []
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0001.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0003.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0005.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0007.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0009.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0011.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0013.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0015.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0017.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0019.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0021.png')))
-        self.run.append(pygame.image.load(
-            os.path.join('raccoon/run', 'run0023.png')))
+        for animation in self.animations.keys():
+            full_path = path + animation
+            self.animations[animation] = import_images_from_folder(full_path)
+
+
+    def animation(self):
+
+
+
+
+
 
         if (self.idle_state == True):
-            self.currentSprite = 0
             self.image = self.idle[self.currentSprite]
-            self.rect = self.image.get_rect()
-            self.rect.bottomleft = (x_pos, y_pos)
+            
         elif (self.run_state == True):
-            self.currentSprite = 0
             self.image = self.run[self.currentSprite]
             self.rect = self.image.get_rect()
             self.rect.bottomleft = (x_pos, y_pos)
@@ -114,7 +73,6 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.rect.left = 900
 
-        self.currentSprite += 0.25
 
         if self.idle_state == True and int(self.currentSprite) >= len(self.idle):
             self.currentSprite = 0
